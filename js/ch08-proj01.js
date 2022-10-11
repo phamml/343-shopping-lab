15b566e5fc974d6877b138e4e8b2c127a582814d
Mia L. Pham <phamml@dukes.jmu.edu>
2022-10-11 10:47:34 -0400

    Implemented tax rate and shipping

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -1,6 +1,21 @@
+function outputTableBody () {
+   const tax_rate = prompt('Enter tax rate (0.10)');
+   const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+   /* add loop and other code here ... in this simple exercise we are not
+      going to concern ourselves with minimizing globals, etc */
+   let sub_total = 0;
+   for (let item of cart) {
+      let total = calculateTotal(item.quantity, item.product.price);
+      sub_total += total;
+      outputCartRow(item, total);
+   }
+   let tax = (tax_rate / 100) * sub_total;
+   let shipping = 40;
 
-/* add loop and other code here ... in this simple exercise we are not
-   going to concern ourselves with minimizing globals, etc */
+   if (shipping_threshold < sub_total) {
+      shipping = 0; 
+   }
+
+   outputTotalRow(tax, shipping, sub_total);
+}
--- js/functions.js
+++ js/functions.js
@@ -21,8 +21,8 @@ function outputCartRow(item, total) {
     document.write(row);
 }
 
-function outputTotalRow(tax, sub_total) {
-    let grand_total = tax + sub_total;
+function outputTotalRow(tax, shipping, sub_total) {
+    let grand_total = tax + sub_total + shipping;
     const row = 
         `<tr class="totals">
             <td colspan="4">Subtotal</td>
@@ -34,7 +34,7 @@ function outputTotalRow(tax, sub_total) {
         </tr>
         <tr class="totals">
             <td colspan="4">Shipping</td>
-            <td>$0.00</td>
+            <td>$${shipping.toFixed(2)}</td>
         </tr>
         <tr class="totals">
             <td colspan="4" class="focus">Grand Total</td>
@@ -44,22 +44,4 @@ function outputTotalRow(tax, sub_total) {
     document.write(row);
     
 }
-
-function outputTableBody() {
-    let grand_total = 0;
-    for (let item of cart) {
-        let total = calculateTotal(item.quantity, item.product.price);
-        grand_total += total;
-        outputCartRow(item, total);
-    }
-    let tax = 55.00;
-    outputTotalRow(tax, grand_total);
-}
-
-
-
-
-
-
-
-        
+      
